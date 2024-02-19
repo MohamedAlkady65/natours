60fe4932dd38e5bd926d9914e0df400148398550
MohamedAlkady <mohamedalkady65@gmail.com>
2024-02-20 01:24:14 +0200

    some work for tour page

--- controller/viewsController.js
+++ controller/viewsController.js
@@ -3,10 +3,18 @@ const catchAsync = require("../utils/catchAsync");
 
 exports.getOverview = catchAsync(async (req, res) => {
 	const tours = await Tour.find();
-	
+
 	res.status(200).render("overview", { title: "All Tour", tours });
 });
 
-exports.getTour = (req, res) => {
-	res.status(200).render("tour", { title: "The Forest Hicker" });
-};
+exports.getTour = catchAsync(async (req, res) => {
+	const slug = req.params.tourSlug;
+	const tour = await Tour.findOne({ slug: slug }).populate({
+		path: "reviews",
+		fields: "review rating user",
+	});
+	res.status(200).render("tour", {
+		title: "The Forest Hicker",
+		tour: tour,
+	});
+});
--- routes/viewsRouter.js
+++ routes/viewsRouter.js
@@ -3,6 +3,6 @@ const viewsController = require("../controller/viewsController");
 const router = express.Router();
 
 router.get("/", viewsController.getOverview);
-router.get("/tour", viewsController.getTour);
+router.get("/tour/:tourSlug", viewsController.getTour);
 
 module.exports = router;
