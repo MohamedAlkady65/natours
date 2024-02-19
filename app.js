5756b7de3a4e6b91f6bd83ad509948c5e422bfa9
MohamedAlkady <mohamedalkady65@gmail.com>
2024-02-19 23:19:40 +0200

    build overview page

--- app.js
+++ app.js
@@ -38,6 +38,7 @@ app.set("views", path.join(__dirname, "/views"));
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public/img")));
 
 // app.use((req, res, next) => {
 // 	console.log("Hello from middleware");
--- controller/viewsController.js
+++ controller/viewsController.js
@@ -1,6 +1,11 @@
-exports.getOverview = (req, res) => {
-	res.status(200).render("overview", { title: "All Tour" });
-};
+const Tour = require("../model/tourModel");
+const catchAsync = require("../utils/catchAsync");
+
+exports.getOverview = catchAsync(async (req, res) => {
+	const tours = await Tour.find();
+	
+	res.status(200).render("overview", { title: "All Tour", tours });
+});
 
 exports.getTour = (req, res) => {
 	res.status(200).render("tour", { title: "The Forest Hicker" });
