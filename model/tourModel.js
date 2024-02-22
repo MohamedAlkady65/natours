8d79bf23e5140b53dadd989096fecbd355c592a9
MohamedAlkady <mohamedalkady65@gmail.com>
2024-02-22 02:23:42 +0200

    apply index

--- model/tourModel.js
+++ model/tourModel.js
@@ -118,6 +118,9 @@ const tourSchema = new mongoose.Schema(
 	}
 );
 
+tourSchema.index({ price: 1, ratingsAverage: -1 });
+tourSchema.index({ slug: 1 });
+
 tourSchema.virtual("durationWeek").get(function () {
 	return Math.ceil(this.duration / 7);
 });
