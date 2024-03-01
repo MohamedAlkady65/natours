c478c96e702d70749fa78b4a18df3ab3c757f76d
MohamedAlkady <mohamedalkady65@gmail.com>
2024-03-01 22:32:30 +0200

    fix stripe webhook 3

--- controller/bookingController.js
+++ controller/bookingController.js
@@ -54,7 +54,7 @@ const createBookingCheckout = async (session) => {
 	await Booking.create({ tour, user, price });
 };
 
-exports.webhookCheckout = async (req, res, next) => {
+exports.webhookCheckout =catchAsync( async (req, res, next) => {
 	const signature = req.headers["stripe-signature"];
 
 	let event;
@@ -72,7 +72,7 @@ exports.webhookCheckout = async (req, res, next) => {
 		await createBookingCheckout(event.data.object);
 
 	res.status(200).json({ received: true });
-};
+});
 
 exports.getMyTours = catchAsync(async (req, res, next) => {
 	const bookings = await Booking.find({ user: req.user.id });
